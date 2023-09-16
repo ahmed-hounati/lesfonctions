d846de310ba2de98f243857a9c2c7c045aaf4cb7
hounati <hounatiahmed@gmail.com>
2023-09-16 10:24:21 +0100

    test

---
+++ lesfonctions/lesfonctions.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//     void signe (int a , int b){
+//         if (a*b>0)
+//             printf("les valeurs de a et b en le meme signe \n");
+//         else
+//             printf("les vaaleurs da a et b ont des signes differents \n");
+
+//     }
+//     int min (int a , int b){
+//         int min ;
+//         if (a>b)
+//             min = b ;
+//         else
+//             min = a ;
+//         return min ;
+//     }
+//     int max (int a , int b){
+//         int max ;
+//         if (a<b)
+//            max = b;
+//         else
+//            max = a;
+//         return max ;
+//     }
+
+// int main()
+// {
+//     int maximal , minimal ;
+//     int a , b ;
+//     printf("veullez entrer la valeur de a : ");
+//     scanf("%d",&a);
+//     printf("vellez entrer la valeur de b : ");
+//     scanf("%d",&b);
+//     signe(a,b);
+//     minimal = min(a,b);
+//     maximal = max(a,b);
+//     printf("la valeur min est : %d \n",minimal);
+//     printf("la valeur max est : %d \n",maximal);
+//     return 0;
+// }
+/////////////////////////////////////////////////////////////////
+// void multi(int n)
+// {
+//     int i ;
+//     printf("La table du multiplication de %d est : \n");
+//     for ( i = 0; i <= 10; i++)
+//         printf("%d x %d = %d \n",n,i,n*i);
+
+// }
+// int main()
+// {
+//     int n;
+//     printf("veullez entrer la valeur de n : ");
+//     scanf("%d", &n);
+//     multi(n);
+//     return 0;
+// }
+////////////////////////////////////////////////////////////////////
+// void bissx (int a)
+// {
+//     if (a%4==0 && a%100!=0 || (a%400==0))
+//       printf("l annee est bissextile");
+//     else
+//         printf("l annee n est pas bissextile ");
+
+// }
+
+// int main()
+// {
+//     int annee;
+//     printf("veullez entrer l annee : ");
+//     scanf("%d",&annee);
+//     bissx(annee);
+
+//     return 0;
+// }
+////////////////////////////////////////////////////
